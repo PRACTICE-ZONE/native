7ae62099c70ed31f24fcab287738601e0704cf0f
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-01 20:04:49 +0300

    Add navigation

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useFonts } from 'expo-font';
+import { StyleSheet, Text, View } from 'react-native';
+import Details from './screens/Details';
+import Home from './screens/Home';
+
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'transparent',
+  },
+};
+
+const App = () => {
+  const [loaded] = useFonts({
+    Roboto: require('./assets/fonts/Roboto-Regular.ttf'),
+    RobotoMedium: require('./assets/fonts/Roboto-Medium.ttf'),
+    RobotoBold: require('./assets/fonts/Roboto-Bold.ttf'),
+  });
+  
   return (
-    <View style={styles.container}>
-      <Text>THIS IS MY FIRST REACT NATIVE APPLICATION Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme = {theme}>
+      <Stack.Navigator screenOptions = {{ headerShown: false }}
+      initialRouteName="Home">
+        <Stack.screen name="Home" component={Home}/>
+
+        <Stack.screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
