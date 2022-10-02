ce0c97d5c6cd11cc6463860bcee8255bcb6d8948
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-02 15:44:39 +0300

    Add focus bar

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native';
+import { FocusStatusBar } from '../components'
+import { COLORS } from '../constants';
 
 const Home = () => {
   return (
-    <Text>Home page</Text>
+    <SafeAreaView style={ { flex: 1} }>
+      <FocusStatusBar background={COLORS.primary} />
+    </SafeAreaView>
   )
 }
 
