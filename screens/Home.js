30c0d2c16d7b0737a5cea700368a54a821c0bd84
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-02 17:47:12 +0300

    Add nftdata

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { View } from 'react-native-web';
 import { FocusStatusBar } from '../components'
-import { COLORS } from '../constants';
+import { COLORS, NFTData } from '../constants';
 
 const Home = () => {
   return (
     <SafeAreaView style={ { flex: 1} }>
       <FocusStatusBar background={COLORS.primary} />
+      <View style={ { flex: 1} } >
+        <View style= { { zIndex: 0 } } >
+          <FlatList data={NFTData} 
+          renderItem={({item}) => <Text>{item.title}</Text>}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
