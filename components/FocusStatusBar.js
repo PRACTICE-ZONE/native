4bf4578af2f3934505f0221de24a93963522489a
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-02 15:52:03 +0300

    Add status bar

--- components/FocusStatusBar.js
+++ components/FocusStatusBar.js
@@ -1,12 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 
-const FocusStatusBar = () => {
-  return (
-    <View>
-      <Text>FocusStatusBar</Text>
-    </View>
-  )
+import React from 'react'
+import { useIsFocused } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+
+const FocusStatusBar = (props) => {
+  const isFocused = useIsFocused();
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
 }
 
 export default FocusStatusBar
