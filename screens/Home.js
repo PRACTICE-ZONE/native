a0a518583b53421712bb08b2c472d49cf649f89f
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-02 18:56:04 +0300

    Add nftdata

--- screens/Home.js
+++ screens/Home.js
@@ -19,7 +19,8 @@ const Home = () => {
         <View style={
           { position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, zIndex: 1 }
         }>
-
+          <View style={{ heigh:300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
         </View>
       </View>
     </SafeAreaView>
