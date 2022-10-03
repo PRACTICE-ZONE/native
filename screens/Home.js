43ad055e09dbec0563f557ceeaf6289db96b3ddf
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-03 14:56:08 +0300

    Display the home

--- screens/Home.js
+++ screens/Home.js
@@ -17,9 +17,9 @@ const Home = () => {
           />
         </View>
         <View style={
-          { position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, zIndex: 1 }
+          { position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, zIndex: -1 }
         }>
-          <View style={{ heigh:300, backgroundColor: COLORS.primary }} />
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
           <View style={{ flex: 1, backgroundColor: COLORS.white }} />
         </View>
       </View>
