8be04186b9948f66cb44a136c1c68e8d740713e6
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-13 00:12:35 +0300

    Add search icon

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,6 +1,6 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
-import { assets, COLORS, SIZES } from '../constants'
+import { assets, COLORS, FONTS, SIZES } from '../constants'
 
 const HomeHeader = () => {
   return (
@@ -33,6 +33,31 @@ const HomeHeader = () => {
           <Image source={assets.person01} resizeMode="contain" style={{ width: "100%", height: "100%" }} />
           <Image source={assets.badge} resizeMode="contain" style={{ position: 'absolute', bottom: 0, right: 0, width: 15, height: 15 }} />
         </View>
+      </View>
+      <View style={{ marginVertical: SIZES.font }}>
+        <Text style = {{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.white,
+        }}>
+          Hello victoria 👋 
+        </Text>
+        <Text style = {{
+          fontFamily: FONTS.bold,
+          fontSize: SIZES.large,
+          color: COLORS.white,
+          marginTop: SIZES.base / 2
+        }}>
+          Let's find a master piece
+        </Text>
+      </View>
+
+      <View style = {{ marginTop: SIZES.font }}>
+        <Image 
+        source={assets.search}
+        resizeMode="contain"
+        style={{ width: 20, height: 20, marginRight: SIZES.base }}
+        />
       </View>
     </View>
   )
