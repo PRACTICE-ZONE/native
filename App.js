6a1bdaa7412ff9aa9d5c7a77c54ac9c61b152b59
IBUNHABIBU <ibunhabib@gmail.com>
2023-03-12 13:29:01 +0300

    Update sdk

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const theme = {
 const App = () => {
   const [loaded] = useFonts({
     InterBold: require('./assets/fonts/Inter-Bold.ttf'),
-    InteSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
     InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
     InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
     InterLight: require('./assets/fonts/Inter-Light.ttf')
